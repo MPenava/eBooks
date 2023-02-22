2b2f9c2a593d95c21b0d51ac8365f04e28290fdf
MPenava <marko.penava@fpmoz.sum.ba>
2023-02-22 22:34:51 +0100

    Navigation drawer work correctly

--- layouts/public.vue
+++ layouts/public.vue
@@ -3,6 +3,11 @@
     <!--Navigation-->
     <v-app-bar color="blue-grey-darken-3">
       <v-container class="d-flex align-center py-0">
+        <v-app-bar-nav-icon
+          class="mr-3"
+          variant="text"
+          @click="drawer = !drawer"
+        ></v-app-bar-nav-icon>
         <v-app-bar-title style="cursor: pointer" @click="$router.push('/')">
           <v-icon
             variant="text"
@@ -14,19 +19,30 @@
         <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
       </v-container>
     </v-app-bar>
+    <v-navigation-drawer v-model="drawer" class="print-hide" app temporary>
+      <v-list>
+        <v-list-item
+          v-for="item in links"
+          :key="item"
+          :value="item"
+          active-color="primary"
+        >
+          <template v-slot:prepend>
+            <v-icon :icon="item.icon"></v-icon>
+          </template>
+
+          <v-list-item-title v-text="item.text"></v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <!--Main-->
     <v-main class="public-bg">
       <v-container>
         <v-row>
           <v-col>
-            <v-sheet
-              min-height="80vh"
-              rounded="lg"
-              class="pa-5"
-              height=100%
-            >
-                <NuxtPage />
+            <v-sheet min-height="80vh" rounded="lg" class="pa-5" height="100%">
+              <NuxtPage />
             </v-sheet>
           </v-col>
         </v-row>
@@ -51,7 +67,10 @@
           <v-icon
             variant="text"
             icon="mdi-book-open-page-variant-outline"
-          ></v-icon> <strong class="text-light">eBooks - {{ new Date().getFullYear() }}</strong>
+          ></v-icon>
+          <strong class="text-light"
+            >eBooks - {{ new Date().getFullYear() }}</strong
+          >
         </v-col>
       </v-row>
     </v-footer>
@@ -61,7 +80,13 @@
 <script>
 export default {
   data: () => ({
-    links: [{text: 'Home', path: '/'},{text: 'Books', path: '/books'}, {text: 'About Us', path: '/about-us'}, {text: 'Administracija', path: '/admin'}]
+    drawer: false,
+    links: [
+      { text: "Home", path: "/", icon: "mdi-home" },
+      { text: "Books", path: "/books", icon: 'mdi-bookshelf'},
+      { text: "About Us", path: "/about-us", icon: 'mdi-information-variant' },
+      { text: "Administracija", path: "/admin", icon: 'mdi-security' },
+    ],
   }),
 };
 </script>
@@ -71,7 +96,6 @@ export default {
   margin: 0;
 
   padding: 0;
-
 }
 
 body {
@@ -85,7 +109,6 @@ a {
 li {
   list-style: none;
 }
-
 
 .public-bg {
   background-image: url("../assets/images/books-bg.jpg");
