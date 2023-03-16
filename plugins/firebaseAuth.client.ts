959f4edd2cb7a9ec589c5da28a4d11c028430f91
MPenava <marko.penava@fpmoz.sum.ba>
2023-03-16 15:03:37 +0100

    Deleting configs for storage and firestore

--- plugins/firebaseAuth.client.ts
+++ plugins/firebaseAuth.client.ts
@@ -1,22 +1,24 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-import {
-  getAuth
-} from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig();
 
   const firebaseConfig = {
     apiKey: config.FIREBASE_API_KEY,
-    databaseURL: "https://ebooks-e4835-default-rtdb.europe-west1.firebasedatabase.app",
+    authDomain: "ebooks-e4835.firebaseapp.com",
+    databaseURL:
+      "https://ebooks-e4835-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "ebooks-e4835",
+    storageBucket: "ebooks-e4835.appspot.com",
+    messagingSenderId: "909873441240",
+    appId: "1:909873441240:web:06bd51b1fd36bb7ec1eaad",
   };
   const app = initializeApp(firebaseConfig);
-  const database = getDatabase(app);
-  
+
   initUser();
 
   const auth = getAuth();
-  nuxtApp.vueApp.provide('auth', auth);
-  nuxtApp.provide('auth', auth);
+  nuxtApp.vueApp.provide("auth", auth);
+  nuxtApp.provide("auth", auth);
 });
